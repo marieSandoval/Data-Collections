a6ec2d8a617b60792b7b5a5351af4d37214723b4
Marie Sandoval <mariejsand@gmail.com>
2023-11-28 19:27:10 -0500

    step 2 and step 3 added

--- ALAB Loops/loops.js
+++ ALAB Loops/loops.js
@@ -43,3 +43,59 @@ for (let i = 1; i < 20; i++) {
   }
 }
 
+/* 
+Part 2: Prime Time
+- Declare an arbitrary number, n.
+- Create a loop that searches for the next prime number, starting at n and incrementing from there.
+- As soon as you find the prime number, log that number and exit the loop.
+*/
+
+//need a count for checking for only two values: a prime number = (1 and itself) if it ever goes above 2 then it is not a prime number
+//to do that we now need to check each number divides by 1 and itself
+
+let num = 23
+
+while (num) {
+    
+    let count = 0
+    let i = 1
+
+    while (i <= num) {
+        if (num % i === 0) {
+            count++
+        }
+        i++
+    }
+
+    if (count === 2) {
+        console.log("This is the prime number: " + num)
+        break
+    }
+    num++
+}
+
+/* 
+Part 3: Feeling Loopy
+Loop through the characters of a given CSV string.
+Store each “cell” of data in a variable.
+When you encounter a comma, move to the next cell.
+When you encounter the “\r\n” sequence, move to the next “row.”
+Log each row of data.
+    -You do not need to format the data, the following works well.
+    -console.log(cell1, cell2, cell3, cell4);
+*/
+
+let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26"
+
+let rows = str.split("\n")
+
+for (row of rows) {
+    const cells = row.split(",")
+
+    const cell1 = cells[0]
+    const cell2 = cells[1]
+    const cell3 = cells[2]
+    const cell4 = cells[3]
+
+    console.log(cell1, cell2, cell3, cell4)
+}
